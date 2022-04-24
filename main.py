74559a3eb94aae1e87f21c9fa4b78ac999497be6
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2022-04-24 21:08:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pathlib
 
 
 
@@ -17,14 +18,25 @@ class MainWindow(QtWidgets.QMainWindow):
         #Load the UI Page
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('task4.ui', self)
-        self.action_open.triggered.connect(self.open)
+        self.time = []
+        self.magnitude = []
+        self.action_open.triggered.connect(self.open)   
     
     def open(self):
-        path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open a file', '','All Files (*.*)')
+        # open any csv file
+        files_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open only CSV ', os.getenv('HOME'), "csv(*.csv)")
+        path = files_name[0]
+        pathlib.Path(path).suffix == ".csv"
+        # read the data in the file
+        data = pd.read_csv(path)
+        # place the data in time and magnitude arrays
+        self.time = data.values[:, 0]
+        self.magnitude = data.values[:, 1]
+        # plot the data
+        self.plotting()      
 
-
-
-   
+    def plotting(self):
+        self.plot_widget.plot(self.time, self.magnitude)
     
     
 app = QtWidgets.QApplication(sys.argv)
